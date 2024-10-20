f4216e14ac8bc56832eed5790ebf040a3a5786f3
Piyush Keshari <piyushkeshari24@gmail.com>
2024-10-20 12:36:42 +0530

    fixed api url

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   const fetchProducts = async () => {
     try {
-      const response = await axios.get('http://api.newrajshreesweets.com/common/products?status=ALL');
+      const response = await axios.get('https://api.newrajshreesweets.com/common/products?status=ALL');
       const products = response.data.data;
 
       // Group products by category and sort products within each category
